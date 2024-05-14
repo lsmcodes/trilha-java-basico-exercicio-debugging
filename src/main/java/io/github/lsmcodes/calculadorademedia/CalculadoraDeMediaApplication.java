4bb0b361bf4703f909f69bbc36d762bebf138689
Lorena Medeiros <lomedeiros@proton.me>
2024-05-14 17:55:03 -0300

    feat: add logic to prompt for student names, calculate and print students' grades average

--- src/main/java/io/github/lsmcodes/calculadorademedia/CalculadoraDeMediaApplication.java
+++ src/main/java/io/github/lsmcodes/calculadorademedia/CalculadoraDeMediaApplication.java
@@ -1,13 +1,46 @@
 package io.github.lsmcodes.calculadorademedia;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculadoraDeMediaApplication {
 
+	private static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(CalculadoraDeMediaApplication.class, args);
+
+		String[] listaEstudantes;
+
+		System.out.println("\nDigite o nome dos estudantes separados por vírgula e sem espaços entre si:");
+		String students = scanner.nextLine();
+
+		listaEstudantes = students.split(",");
+
+		double media = calcularMedia(listaEstudantes);
+
+		System.out.printf("\nMédia dos alunos: %.1f.", media);
+
+	}
+
+	public static double calcularMedia(String[] estudantes) {
+
+		double total = 0;
+		Integer quatidadeEstudantes = estudantes.length;
+
+		for(String estudante : estudantes) {
+			System.out.printf("\nDigite a nota de %s:", estudante);
+
+			double nota = scanner.nextDouble();
+			total += nota;
+		}
+
+		return total / quatidadeEstudantes;
+
 	}
 
 }
